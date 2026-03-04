89ae0735923202479ab91ebd4b9ef91d533162c4
LorenzMayr <156314024+LorenzMayr@users.noreply.github.com>
2026-03-04 09:22:12 +0100

    Placeholder IP-Address

--- netbox_script.py
+++ netbox_script.py
@@ -8,7 +8,6 @@ class PushToAnsible(Script):
         device = data["device"]
 
         # Get the rendered config from NetBox
-        # Get the rendered config from NetBox
         context_data = device.get_config_context()
         context_data.update({'device': device})
         config_template = device.get_config_template()
@@ -20,8 +19,9 @@ class PushToAnsible(Script):
             "rendered_config": rendered_config
         }
 
-        # POST the payload to an Ansible endpoint (e.g., your local server)
-        response = requests.post("http://10.201.145.251:8080/configure", json=payload)
+        # POST the payload to the Ansible endpoint
+        response = requests.post("http://10.10.10.10:8080/configure", json=payload) # Replace the IP with your Ansible IP-Address
+                                                                                    # Port and Endpoint stay the same
 
         # Log the response from Ansible
         self.log_info(f"Config sent to Ansible. Status: {response.status_code}")
